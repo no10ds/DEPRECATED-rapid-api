daa2d997d1797b9b80aae335e1e5cbb2c44c7d9c
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-07-22 09:01:21 +0100

    167-CP Use f-strings

--- api/common/config/auth.py
+++ api/common/config/auth.py
@@ -7,46 +7,31 @@ from api.common.utilities import BaseEnum
 
 RAPID_ACCESS_TOKEN = "rat"  # nosec B105
 COOKIE_MAX_AGE_IN_SECONDS = 3600
+
 DEFAULT_SCOPE = ["READ_PUBLIC"]
+
 COGNITO_ALLOWED_FLOWS = ["client_credentials"]
-COGNITO_RESOURCE_SERVER_ID = "https://" + DOMAIN_NAME
+COGNITO_RESOURCE_SERVER_ID = f"https://{DOMAIN_NAME}"
 COGNITO_USER_POOL_ID = os.environ["COGNITO_USER_POOL_ID"]
-IDENTITY_PROVIDER_TOKEN_URL = (
-    "https://"
-    + RESOURCE_PREFIX
-    + "-auth.auth."
-    + AWS_REGION
-    + ".amazoncognito.com/oauth2/token"
-)
-IDENTITY_PROVIDER_AUTHORIZATION_URL = (
-    "https://"
-    + RESOURCE_PREFIX
-    + "-auth.auth."
-    + AWS_REGION
-    + ".amazoncognito.com/oauth2/authorize"
-)
-COGNITO_JWKS_URL = (
-    "https://cognito-idp."
-    + AWS_REGION
-    + ".amazonaws.com/"
-    + COGNITO_USER_POOL_ID
-    + "/.well-known/jwks.json"
-)
-COGNITO_EXPLICIT_AUTH_FLOWS = [
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_CUSTOM_AUTH",
-    "ALLOW_USER_SRP_AUTH",
-]
+
+IDENTITY_PROVIDER_TOKEN_URL = f"https://{RESOURCE_PREFIX}-auth.auth.{AWS_REGION}.amazoncognito.com/oauth2/token"
+IDENTITY_PROVIDER_AUTHORIZATION_URL = f"https://{RESOURCE_PREFIX}-auth.auth.{AWS_REGION}.amazoncognito.com/oauth2/authorize"
+COGNITO_JWKS_URL = f"https://cognito-idp.{AWS_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
+
+COGNITO_EXPLICIT_AUTH_FLOWS = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_USER_SRP_AUTH"]
 
 COGNITO_USER_LOGIN_APP_CREDENTIALS_SECRETS_NAME = os.getenv(
-    "COGNITO_USER_LOGIN_APP_CREDENTIALS_SECRETS_NAME", "rapid_cognito_user_secrets"
+    "COGNITO_USER_LOGIN_APP_CREDENTIALS_SECRETS_NAME",
+    "rapid_cognito_user_secrets"
 )
+
 COGNITO_REDIRECT_URI = f"https://{DOMAIN_NAME}/oauth2/success"
-PROTECTED_DOMAIN_SCOPES_PARAMETER_NAME = RESOURCE_PREFIX + "_protected_domain_scopes"
+PROTECTED_DOMAIN_SCOPES_PARAMETER_NAME = f"{RESOURCE_PREFIX}_protected_domain_scopes"
 
 
 def construct_user_auth_url(client_id: str):
-    return f"{IDENTITY_PROVIDER_AUTHORIZATION_URL}?client_id={client_id}&response_type=code&redirect_uri={urllib.parse.quote_plus(COGNITO_REDIRECT_URI)}"
+    redirect_uri = urllib.parse.quote_plus(COGNITO_REDIRECT_URI)
+    return f"{IDENTITY_PROVIDER_AUTHORIZATION_URL}?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}"
 
 
 class Action(BaseEnum):
--- api/common/data_parsers.py
+++ api/common/data_parsers.py
@@ -4,7 +4,7 @@ from typing import List
 
 def parse_categorisation(
     path: str, categories: List[str], category_name: str = "categorisation"
-):
+) -> str:
     match = re.findall(rf"({'|'.join(categories)})", path)
     if match:
         return match[0]
