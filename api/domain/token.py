645255e92196344d66e0282c2c3b878f33040234
Cristhian Yeison Da Silva Fernandez <cristhian.d@thoughtworks.com>
2022-07-28 17:53:25 +0100

    329-CD Add bandit to hooks

--- api/domain/token.py
+++ api/domain/token.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from api.common.config.auth import COGNITO_RESOURCE_SERVER_ID
+from api.common.config.auth import COGNITO_RESOURCE_SERVER_ID, SubjectType
 
 
 class Token:
@@ -10,10 +10,10 @@ class Token:
         self.permissions: List[str] = self._extract_permissions(payload)
 
     def is_client_token(self) -> bool:
-        return self.token_type == "CLIENT"
+        return self.token_type == SubjectType.CLIENT.value
 
     def is_user_token(self) -> bool:
-        return self.token_type == "USER"
+        return self.token_type == SubjectType.USER.value
 
     def _extract_subject(self, payload: dict) -> str:
         try:
@@ -31,11 +31,11 @@ class Token:
         permission_scopes = payload.get("scope", None)
 
         if permission_groups:
-            self.token_type = "USER"
+            self.token_type = SubjectType.USER.value
             return permission_groups
 
         if permission_scopes:
-            self.token_type = "CLIENT"
+            self.token_type = SubjectType.CLIENT.value
             try:
                 scopes = payload["scope"].split()
                 return [
