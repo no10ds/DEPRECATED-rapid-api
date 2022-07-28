6d070df3361a139dcd56748f54adae3088505a65
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-07-28 14:42:13 +0100

    348-CP Re-implement unauthorised flows E2E tests

--- test/e2e/test_journey.py
+++ test/e2e/test_journey.py
@@ -1,5 +1,6 @@
 import json
 from abc import ABC
+from http import HTTPStatus
 
 import boto3
 import requests
@@ -48,22 +49,22 @@ class TestUnauthenticatedJourneys(BaseJourneyTest):
     def test_status_always_accessible(self):
         api_url = f"{self.base_url}/status"
         response = requests.get(api_url)
-        assert response.status_code == 200
+        assert response.status_code == HTTPStatus.OK
 
     def test_query_is_forbidden_when_no_token_provided(self):
         url = self.query_dataset_url("mydomain", "unknowndataset")
         response = requests.post(url)
-        assert response.status_code == 403
+        assert response.status_code == HTTPStatus.FORBIDDEN
 
     def test_upload_is_forbidden_when_no_token_provided(self):
         files = {"file": (self.filename, open("./test/e2e/" + self.filename, "rb"))}
         url = self.upload_dataset_url("test_e2e", "upload")
         response = requests.post(url, files=files)
-        assert response.status_code == 403
+        assert response.status_code == HTTPStatus.FORBIDDEN
 
     def test_list_is_forbidden_when_no_token_provided(self):
         response = requests.post(self.datasets_url)
-        assert response.status_code == 403
+        assert response.status_code == HTTPStatus.FORBIDDEN
 
 
 class TestUnauthorisedJourney(BaseJourneyTest):
