5e11affaf9e2d6018422405b2bd5d7870ce079bb
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-07-29 11:20:07 +0100

    348-CP Refactor E2E tests
    Among other changes to make the tests more consistent we have also pulled the setup logic into the tests that require it

--- test/e2e/test_journey.py
+++ test/e2e/test_journey.py
@@ -3,7 +3,6 @@ from abc import ABC
 from http import HTTPStatus
 
 import boto3
-import pytest
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -13,39 +12,37 @@ from test.e2e.e2e_test_utils import get_secret, AuthenticationFailedError
 
 
 class BaseJourneyTest(ABC):
-    s3_client = boto3.client("s3")
-
     base_url = f"https://{DOMAIN_NAME}"
-    datasets_url = f"{base_url}/datasets"
+    datasets_endpoint = f"{base_url}/datasets"
 
-    data_directory = "data/test_e2e"
-    raw_data_directory = "raw_data/test_e2e"
+    e2e_test_domain = "test_e2e"
+
+    schemas_directory = "data/schemas/PUBLIC"
+    data_directory = f"data/{e2e_test_domain}"
+    raw_data_directory = f"raw_data/{e2e_test_domain}"
 
     filename = "test_journey_file.csv"
 
-    data_filepath = f"{data_directory}"
-    raw_data_filepath = f"{raw_data_directory}"
-
     def upload_dataset_url(self, domain: str, dataset: str) -> str:
-        return f"{self.datasets_url}/{domain}/{dataset}"
+        return f"{self.datasets_endpoint}/{domain}/{dataset}"
 
     def query_dataset_url(self, domain: str, dataset: str) -> str:
-        return f"{self.datasets_url}/{domain}/{dataset}/query"
+        return f"{self.datasets_endpoint}/{domain}/{dataset}/query"
 
     def info_dataset_url(self, domain: str, dataset: str) -> str:
-        return f"{self.datasets_url}/{domain}/{dataset}/info"
+        return f"{self.datasets_endpoint}/{domain}/{dataset}/info"
 
     def list_dataset_files_url(self, domain: str, dataset: str) -> str:
-        return f"{self.datasets_url}/{domain}/{dataset}/files"
+        return f"{self.datasets_endpoint}/{domain}/{dataset}/files"
 
     def delete_data_url(self, domain: str, dataset: str, filename: str) -> str:
-        return f"{self.datasets_url}/{domain}/{dataset}/{filename}"
+        return f"{self.datasets_endpoint}/{domain}/{dataset}/{filename}"
 
     def status_url(self) -> str:
         return f"{self.base_url}/status"
 
 
-class TestUnauthenticatedJourneys(BaseJourneyTest):
+class TestGeneralBehaviour(BaseJourneyTest):
     def test_http_request_is_redirected_to_https(self):
         response = requests.get(self.status_url())
         assert f"https://{DOMAIN_NAME}" in response.url
@@ -55,6 +52,8 @@ class TestUnauthenticatedJourneys(BaseJourneyTest):
         response = requests.get(api_url)
         assert response.status_code == HTTPStatus.OK
 
+
+class TestUnauthenticatedJourneys(BaseJourneyTest):
     def test_query_is_forbidden_when_no_token_provided(self):
         url = self.query_dataset_url("mydomain", "unknowndataset")
         response = requests.post(url)
@@ -62,95 +61,36 @@ class TestUnauthenticatedJourneys(BaseJourneyTest):
 
     def test_upload_is_forbidden_when_no_token_provided(self):
         files = {"file": (self.filename, open("./test/e2e/" + self.filename, "rb"))}
-        url = self.upload_dataset_url("test_e2e", "upload")
+        url = self.upload_dataset_url(self.e2e_test_domain, "upload")
         response = requests.post(url, files=files)
         assert response.status_code == HTTPStatus.FORBIDDEN
 
     def test_list_is_forbidden_when_no_token_provided(self):
-        response = requests.post(self.datasets_url)
+        response = requests.post(self.datasets_endpoint)
         assert response.status_code == HTTPStatus.FORBIDDEN
 
 
 class TestUnauthorisedJourney(BaseJourneyTest):
-    token_url = f"https://{DOMAIN_NAME}/oauth2/token"
-    credentials = get_secret(
-        secret_name="E2E_TEST_COGNITO_APP_CLIENT_ID_AND_SECRET"  # pragma: allowlist secret
-    )
-    cognito_client_id = credentials["CLIENT_ID"]
-    cognito_client_secret = credentials["CLIENT_SECRET"]  # pragma: allowlist secret
-
-    access_token = None
-
     def setup_class(self):
-        auth = HTTPBasicAuth(self.cognito_client_id, self.cognito_client_secret)
+        token_url = f"https://{DOMAIN_NAME}/oauth2/token"
+
+        credentials = get_secret(
+            secret_name="E2E_TEST_COGNITO_APP_CLIENT_ID_AND_SECRET"  # pragma: allowlist secret
+        )
+        cognito_client_id = credentials["CLIENT_ID"]
+        cognito_client_secret = credentials["CLIENT_SECRET"]  # pragma: allowlist secret
+
+        auth = HTTPBasicAuth(cognito_client_id, cognito_client_secret)
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         payload = {
             "grant_type": "client_credentials",
-            "client_id": self.cognito_client_id,
+            "client_id": cognito_client_id,
             "scope": f"https://{DOMAIN_NAME}/WRITE_ALL",
         }
 
-        response = requests.post(
-            self.token_url, auth=auth, headers=headers, json=payload
-        )
-
-        if response.status_code != 200:
-            raise AuthenticationFailedError(f"{response.status_code}")
-
-        self.token = json.loads(response.content.decode(CONTENT_ENCODING))[
-            "access_token"
-        ]
-
-    def generate_auth_headers(self):
-        return {"Authorization": f"Bearer {self.token}"}
-
-    def test_query_existing_dataset_when_not_authorised_to_read(self):
-        url = self.query_dataset_url("test_e2e", "query")
-        response = requests.post(url, headers=self.generate_auth_headers())
-        assert response.status_code == HTTPStatus.UNAUTHORIZED
-
-    def test_existing_dataset_info_when_not_authorised_to_read(self):
-        url = self.info_dataset_url("test_e2e", "query")
-        response = requests.get(url, headers=self.generate_auth_headers())
-        assert response.status_code == HTTPStatus.UNAUTHORIZED
-
-    @pytest.mark.skip(
-        "This behaviour has now changed. the WRITE_ALL permission would now allow the user to delete ANY dataset"
-    )
-    def test_delete_file_when_not_authorised_to_delete(self):
-        url = self.delete_data_url(
-            domain="test_e2e", dataset="delete", filename="test_journey_file.csv"
-        )
-        response = requests.delete(url, headers=self.generate_auth_headers())
-        assert response.status_code == HTTPStatus.UNAUTHORIZED
-
-
-class TestAuthenticatedJourneys(BaseJourneyTest):
-    token_url = f"https://{DOMAIN_NAME}/oauth2/token"
-    credentials = get_secret(
-        secret_name="E2E_TEST_COGNITO_APP_CLIENT_ID_AND_SECRET"  # pragma: allowlist secret
-    )
-    cognito_client_id = credentials["CLIENT_ID"]
-    cognito_client_secret = credentials["CLIENT_SECRET"]  # pragma: allowlist secret
-
-    access_token = None
-
-    def setup_class(self):
-        auth = HTTPBasicAuth(self.cognito_client_id, self.cognito_client_secret)
-
-        headers = {"Content-Type": "application/x-www-form-urlencoded"}
-
-        payload = {
-            "grant_type": "client_credentials",
-            "client_id": self.cognito_client_id,
-            "scope": f"https://{DOMAIN_NAME}/READ_ALL https://{DOMAIN_NAME}/WRITE_ALL",
-        }
-
-        response = requests.post(
-            self.token_url, auth=auth, headers=headers, json=payload
-        )
+        response = requests.post(token_url, auth=auth, headers=headers, json=payload)
 
         if response.status_code != HTTPStatus.OK:
             raise AuthenticationFailedError(f"{response.status_code}")
@@ -159,71 +99,130 @@ class TestAuthenticatedJourneys(BaseJourneyTest):
             "access_token"
         ]
 
-        self.s3_client.put_object(
-            Bucket=DATA_BUCKET,
-            Key=f"{self.raw_data_filepath}/delete/{self.filename}",
-            Body=open("./test/e2e/" + self.filename, "rb"),
+    # Utils -------------
+
+    def generate_auth_headers(self):
+        return {"Authorization": f"Bearer {self.token}"}
+
+    # Tests -------------
+
+    def test_query_existing_dataset_when_not_authorised_to_read(self):
+        url = self.query_dataset_url(self.e2e_test_domain, "query")
+        response = requests.post(url, headers=self.generate_auth_headers())
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+    def test_existing_dataset_info_when_not_authorised_to_read(self):
+        url = self.info_dataset_url(self.e2e_test_domain, "query")
+        response = requests.get(url, headers=self.generate_auth_headers())
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+class TestAuthenticatedJourneys(BaseJourneyTest):
+    s3_client = boto3.client("s3")
+
+    def setup_class(self):
+        token_url = f"https://{DOMAIN_NAME}/oauth2/token"
+
+        credentials = get_secret(
+            secret_name="E2E_TEST_COGNITO_APP_CLIENT_ID_AND_SECRET"  # pragma: allowlist secret
         )
 
-        self.s3_client.put_object(
-            Bucket=DATA_BUCKET,
-            Key=f"{self.data_filepath}/delete/{self.filename}",
-            Body=open("./test/e2e/" + self.filename, "rb"),
-        )
+        cognito_client_id = credentials["CLIENT_ID"]
 
-    def teardown_class(self):
+        cognito_client_secret = credentials["CLIENT_SECRET"]  # pragma: allowlist secret
+
+        auth = HTTPBasicAuth(cognito_client_id, cognito_client_secret)
+
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": cognito_client_id,
+            "scope": f"https://{DOMAIN_NAME}/READ_ALL https://{DOMAIN_NAME}/WRITE_ALL",
+        }
+
+        response = requests.post(token_url, auth=auth, headers=headers, json=payload)
+
+        if response.status_code != HTTPStatus.OK:
+            raise AuthenticationFailedError(f"{response.status_code}")
+
+        self.token = json.loads(response.content.decode(CONTENT_ENCODING))[
+            "access_token"
+        ]
+
+    # Utils -------------
+
+    def generate_auth_headers(self):
+        return {"Authorization": f"Bearer {self.token}"}
+
+    def delete_all_data_and_raw_files_for_(self, domain: str, filetype: str = ".csv"):
         files = self.s3_client.list_objects_v2(
-            Bucket=DATA_BUCKET, Prefix=f"{self.raw_data_filepath}/upload"
+            Bucket=DATA_BUCKET, Prefix=f"{self.raw_data_directory}/{domain}"
         )
 
         files_to_delete = [
             file["Key"].rsplit("/", 1)[-1]
             for file in files["Contents"]
-            if file["Key"].endswith(".csv")
+            if file["Key"].endswith(filetype)
         ]
 
         filepaths_to_delete = []
+
         for filename in files_to_delete:
             filepaths_to_delete.append(
-                {"Key": f"{self.raw_data_filepath}/upload/{filename}"}
+                {"Key": f"{self.raw_data_directory}/{domain}/{filename}"}
             )
             filepaths_to_delete.append(
-                {"Key": f"{self.data_filepath}/upload/{filename}"}
+                {"Key": f"{self.data_directory}/{domain}/{filename}"}
             )
 
         self.s3_client.delete_objects(
             Bucket=DATA_BUCKET, Delete={"Objects": filepaths_to_delete}
         )
 
-    def generate_auth_headers(self):
-        return {"Authorization": f"Bearer {self.token}"}
+    def upload_test_file_to_(self, data_directory: str, domain: str):
+        self.s3_client.put_object(
+            Bucket=DATA_BUCKET,
+            Key=f"{data_directory}/{domain}/{self.filename}",
+            Body=open("./test/e2e/" + self.filename, "rb"),
+        )
 
-    def test_query_non_existing_dataset_when_authorised(self):
-        url = self.query_dataset_url("mydomain", "unknowndataset")
-        response = requests.post(url, headers=self.generate_auth_headers())
-        assert response.status_code == HTTPStatus.BAD_REQUEST
-
-    def test_upload_when_authorised(self):
-        files = {"file": (self.filename, open("./test/e2e/" + self.filename, "rb"))}
-        url = self.upload_dataset_url("test_e2e", "upload")
-        response = requests.post(url, headers=self.generate_auth_headers(), files=files)
-        assert response.status_code == HTTPStatus.CREATED
+    # Tests -------------
 
     def test_list_when_authorised(self):
         response = requests.post(
-            self.datasets_url,
+            self.datasets_endpoint,
             headers=self.generate_auth_headers(),
             json={"tags": {"test": "e2e"}},
         )
         assert response.status_code == HTTPStatus.OK
 
-    def test_query_existing_dataset_when_authorised(self):
-        url = self.query_dataset_url(domain="test_e2e", dataset="query")
+    def test_uploads_when_authorised(self):
+        files = {"file": (self.filename, open("./test/e2e/" + self.filename, "rb"))}
+        url = self.upload_dataset_url(self.e2e_test_domain, "upload")
+        response = requests.post(url, headers=self.generate_auth_headers(), files=files)
+
+        assert response.status_code == HTTPStatus.CREATED
+
+        self.delete_all_data_and_raw_files_for_(domain="upload", filetype=".csv")
+
+    def test_gets_existing_dataset_info_when_authorised(self):
+        url = self.info_dataset_url(domain=self.e2e_test_domain, dataset="query")
+        response = requests.get(url, headers=(self.generate_auth_headers()))
+        assert response.status_code == HTTPStatus.OK
+
+    def test_queries_non_existing_dataset_when_authorised(self):
+        url = self.query_dataset_url("mydomain", "unknowndataset")
+        response = requests.post(url, headers=self.generate_auth_headers())
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+    def test_queries_existing_dataset_as_json_when_authorised(self):
+        url = self.query_dataset_url(domain=self.e2e_test_domain, dataset="query")
         response = requests.post(url, headers=(self.generate_auth_headers()))
         assert response.status_code == HTTPStatus.OK
 
-    def test_query_existing_dataset_as_csv_when_authorised(self):
-        url = self.query_dataset_url(domain="test_e2e", dataset="query")
+    def test_queries_existing_dataset_as_csv_when_authorised(self):
+        url = self.query_dataset_url(domain=self.e2e_test_domain, dataset="query")
         headers = {
             "Accept": "text/csv",
             "Authorization": "Bearer " + self.token,
@@ -237,20 +236,19 @@ class TestAuthenticatedJourneys(BaseJourneyTest):
             + '1,"2017","7","Darlington","Durham","regular","completed"\n'
         )
 
-    def test_get_existing_dataset_info_when_authorised(self):
-        url = self.info_dataset_url(domain="test_e2e", dataset="query")
-        response = requests.get(url, headers=(self.generate_auth_headers()))
-        assert response.status_code == HTTPStatus.OK
-
     def test_fails_to_query_when_authorised_and_sql_injection_attempted(self):
-        url = self.query_dataset_url(domain="test_e2e", dataset="query")
+        url = self.query_dataset_url(domain=self.e2e_test_domain, dataset="query")
         body = {"filter": "';DROP TABLE test_e2e--"}
         response = requests.post(url, headers=(self.generate_auth_headers()), json=body)
         assert response.status_code == HTTPStatus.FORBIDDEN
 
-    def test_delete_existing_data_when_authorised(self):
+    def test_deletes_existing_data_when_authorised(self):
+        # Upload files directly to relevant directories in S3
+        self.upload_test_file_to_(self.raw_data_directory, domain="delete")
+        self.upload_test_file_to_(self.data_directory, domain="delete")
+
         # Get available datasets
-        url = self.list_dataset_files_url(domain="test_e2e", dataset="delete")
+        url = self.list_dataset_files_url(domain=self.e2e_test_domain, dataset="delete")
         available_datasets_response = requests.get(
             url, headers=(self.generate_auth_headers())
         )
@@ -262,7 +260,7 @@ class TestAuthenticatedJourneys(BaseJourneyTest):
         # Delete chosen dataset
         first_dataset_file = response_list[0]
         url2 = self.delete_data_url(
-            domain="test_e2e", dataset="delete", filename=first_dataset_file
+            domain=self.e2e_test_domain, dataset="delete", filename=first_dataset_file
         )
         response2 = requests.delete(url2, headers=(self.generate_auth_headers()))
 
