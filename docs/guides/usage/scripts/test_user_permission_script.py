b749f188fb3d8869bcde7fa546c987edca0ad42b
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2022-07-29 15:02:04 +0100

    309-LA/SD Fix test user permission script to access create client and list datasets endpoint

--- docs/guides/usage/scripts/test_user_permission_script.py
+++ docs/guides/usage/scripts/test_user_permission_script.py
@@ -27,9 +27,6 @@ def fetch_user_token():
     return json.loads(response.content.decode("utf-8"))["access_token"]
 
 
-cookie = ""
-
-
 def fetch_dataset(token: str, domain: str, dataset: str):
     post_url = f"{BASE_URL}/datasets/{domain}/{dataset}/query"
     headers = {"Accept": "text/html...", "cookie": f"rat={token}"}
@@ -38,4 +35,25 @@ def fetch_dataset(token: str, domain: str, dataset: str):
     return response.status_code, json.loads(response.content.decode("utf-8"))
 
 
-fetch_dataset(cookie, DOMAIN, DATASET)
+def list_all_datasets(token: str):
+    post_url = f"{BASE_URL}/datasets"
+    headers = {"Accept": "text/html...", "cookie": f"rat={token}"}
+    response = requests.post(post_url, headers=headers)
+    print(response.status_code)
+    print(response.content.decode("utf-8"))
+    return response.status_code, json.loads(response.content.decode("utf-8"))
+
+
+def create_client(token: str):
+    post_url = f"{BASE_URL}/client"
+    headers = {"Accept": "text/html...", "cookie": f"rat={token}"}
+    body = {"client_name": "test_client_2907", "permissions": ["READ_PUBLIC"]}
+    response = requests.post(post_url, data=json.dumps(body), headers=headers)
+    print(response.status_code)
+    print(response.content.decode("utf-8"))
+    return response.status_code, json.loads(response.content.decode("utf-8"))
+
+
+cookie = None
+create_client(cookie)
+list_all_datasets(cookie)
