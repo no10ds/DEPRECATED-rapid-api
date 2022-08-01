d796bc4423290c84fa2704bc41efdeb595a5453f
Lydiaade <lydia.adejumo@thoughtworks.com>
2022-08-01 11:24:28 +0100

    309-JP/LA Test non-existing user access in permissions script

--- docs/guides/usage/scripts/test_user_permission_script.py
+++ docs/guides/usage/scripts/test_user_permission_script.py
@@ -1,30 +1,9 @@
 import json
 
 import requests
-from requests.auth import HTTPBasicAuth
 
-username = None
-password = None
 BASE_URL = "http://0.0.0.0:8000"
 DOMAIN = "domain"
-DATASET = "dataset"
-
-
-def fetch_user_token():
-    auth = HTTPBasicAuth(username, password)
-
-    payload = {
-        "grant_type": "client_credentials",
-        "client_id": username,
-    }
-
-    headers = None
-
-    response = requests.post(
-        BASE_URL + "/oauth2/token", auth=auth, headers=headers, json=payload
-    )
-
-    return json.loads(response.content.decode("utf-8"))["access_token"]
 
 
 def fetch_dataset(token: str, domain: str, dataset: str):
@@ -44,6 +23,14 @@ def list_all_datasets(token: str):
     return response.status_code, json.loads(response.content.decode("utf-8"))
 
 
+def list_all_datasets_no_token():
+    post_url = f"{BASE_URL}/datasets"
+    response = requests.post(post_url)
+    print(response.status_code)
+    print(response.content.decode("utf-8"))
+    return response.status_code, json.loads(response.content.decode("utf-8"))
+
+
 def create_client(token: str):
     post_url = f"{BASE_URL}/client"
     headers = {"Accept": "text/html...", "cookie": f"rat={token}"}
@@ -55,5 +42,6 @@ def create_client(token: str):
 
 
 cookie = None
-create_client(cookie)
-list_all_datasets(cookie)
+# create_client(cookie)
+# list_all_datasets(cookie)
+# list_all_datasets_no_token()
