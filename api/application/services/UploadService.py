5f62be7f195100897d83ec470642aa07a3545787
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2022-08-05 17:04:30 +0100

    356-JP/SD [WIP] Handle protected permission

--- api/application/services/UploadService.py
+++ api/application/services/UploadService.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import Set, Dict
 
 from api.adapter.aws_resource_adapter import AWSResourceAdapter
 from api.adapter.dynamodb_adapter import DynamoDBAdapter
@@ -17,34 +17,54 @@ class UploadService:
         sensitivities_and_domains = self._extract_sensitivities_and_domains(subject_id)
         return self._fetch_datasets(sensitivities_and_domains)
 
-    def _fetch_datasets(self, sensitivities_and_domains):
+    def _fetch_datasets(self, sensitivities_and_domains: Dict[str, Set[str]]):
         authorised_datasets = set()
         datasets_metadata_list = []
-        for sensitivity in sensitivities_and_domains:
+        for sensitivity in sensitivities_and_domains.get("sensitivities"):
             query = DatasetFilters(sensitivity=sensitivity)
             datasets_metadata_list.extend(
                 self.resource_adapter.get_datasets_metadata(query)
             )
+        # for protected_domain in sensitivities_and_domains.get("protected_domains"):
+        #     query = DatasetFilters(sensitivity=SensitivityLevel.PROTECTED.value)
+        #     datasets_metadata_list.extend(
+        #         self.resource_adapter.get_datasets_metadata(query)
+        #     )
 
         [
             authorised_datasets.add(datasets_metadata.dataset)
             for datasets_metadata in datasets_metadata_list
+            if datasets_metadata.tags.get("sensitivity")
+            == SensitivityLevel.PROTECTED.value
+            and datasets_metadata.domain
+            in sensitivities_and_domains.get("protected_domains")
         ]
         return authorised_datasets
 
-    def _extract_sensitivities_and_domains(self, subject_id) -> Set[str]:
+    def _extract_sensitivities_and_domains(self, subject_id) -> Dict[str, Set[str]]:
         start_index = len(Action.WRITE.value + "_")
+        start_index_for_protected = len(
+            Action.WRITE.value + "_" + SensitivityLevel.PROTECTED.value + "_"
+        )
         permissions = self.dynamodb_adapter.get_permissions_for_subject(subject_id)
-        sensitivities_and_domains = set()
+        sensitivities = set()
+        protected_domains = set()
+        # {"domain": set(), "sensitivities": set()}
         for permission in permissions:
             if permission == Action.WRITE.value + "_ALL":
-                sensitivities_and_domains.update(SensitivityLevel.get_all_values())
+                sensitivities.update(SensitivityLevel.get_all_values())
+                break
             elif (
                 permission == Action.WRITE.value + "_" + SensitivityLevel.PRIVATE.value
             ):
-                sensitivities_and_domains.update(
+                sensitivities.update(
                     [SensitivityLevel.PRIVATE.value, SensitivityLevel.PUBLIC.value]
                 )
+            elif permission.startswith(
+                Action.WRITE.value + "_" + SensitivityLevel.PROTECTED.value + "_"
+            ):
+                sensitivities.add(SensitivityLevel.PROTECTED.value)
+                protected_domains.add(permission[start_index_for_protected:])
             elif permission.startswith(Action.WRITE.value):
-                sensitivities_and_domains.add(permission[start_index:])
-        return sensitivities_and_domains
+                sensitivities.add(permission[start_index:])
+        return {"protected_domains": protected_domains, "sensitivities": sensitivities}
--- test/api/application/services/test_upload_service.py
+++ test/api/application/services/test_upload_service.py
@@ -102,6 +102,33 @@ class TestUploadService:
         assert "test_dataset_1" in result
         assert mock_get_datasets_metadata.call_count == 1
         mock_get_permissions_for_subject.assert_called_once_with(subject_id)
-        mock_get_datasets_metadata.assert_has_calls(
-            [call(query_public)], any_order=True
+        mock_get_datasets_metadata.assert_called_once_with(query_public)
+
+    @patch.object(AWSResourceAdapter, "get_datasets_metadata")
+    @patch.object(DynamoDBAdapter, "get_permissions_for_subject")
+    def test_get_authorised_datasets_for_protected_domain(
+        self, mock_get_permissions_for_subject, mock_get_datasets_metadata
+    ):
+        subject_id = "1234adsfasd8234kj"
+        permissions = ["READ_ALL", "WRITE_PROTECTED_TESTDOMAIN"]
+        query_protected = DatasetFilters(sensitivity="PROTECTED")
+        enriched_dataset_metadata_1 = AWSResourceAdapter.EnrichedDatasetMetaData(
+            dataset="test_dataset_1", domain="TESTDOMAIN"
         )
+        enriched_dataset_metadata_2 = AWSResourceAdapter.EnrichedDatasetMetaData(
+            dataset="test_dataset_2", domain="ANOTHERDOMAIN"
+        )
+        enriched_dataset_metadata_list = [
+            enriched_dataset_metadata_1,
+            enriched_dataset_metadata_2,
+        ]
+        mock_get_permissions_for_subject.return_value = permissions
+        mock_get_datasets_metadata.return_value = enriched_dataset_metadata_list
+
+        result = self.upload_service.get_authorised_datasets(subject_id)
+
+        assert len(result) == 1
+        assert "test_dataset_1" in result
+        assert mock_get_datasets_metadata.call_count == 1
+        mock_get_permissions_for_subject.assert_called_once_with(subject_id)
+        mock_get_datasets_metadata.assert_called_once_with(query_protected)
