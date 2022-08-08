43dd87d4b940cbbbf3481f470c063036d6d4d00d
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-08-08 10:46:04 +0100

    328-CD/CP Add login/logout sequence journey test

--- test/ui/test_ui.py
+++ test/ui/test_ui.py
@@ -4,6 +4,8 @@ from abc import ABC
 
 from playwright.sync_api import sync_playwright, expect
 
+from test.test_utils import get_secret
+
 DOMAIN_NAME = os.environ["DOMAIN_NAME"]
 BASE_URL = f"https://{DOMAIN_NAME}"
 BROWSER_MODE = os.getenv("BROWSER_MODE", "HEADLESS")
@@ -25,17 +27,60 @@ class BaseTestUI(ABC):
         print(f"Clicking on link: {link_text}")
         page.click(f"//a[text()='{link_text}']")
 
+    def click_button(self, page, button_text: str):
+        print(f"Clicking button: {button_text}")
+        page.click(f"//button[text()='{button_text}']")
+
     def assert_title(self, page, title: str):
         print(f"Checking page title: {title}")
         expect(page).to_have_title(re.compile(title))
 
+    def input_text_value(self, page, input_id: str, value: str):
+        print(f"Typing {value} into input with ID {input_id}")
+        page.locator(f"//input[@id='{input_id}']").fill(value)
+
+    def login_with_cognito(self, page):
+        print("Logging test user in with Cognito")
+
+        test_user_credentials = get_secret(secret_name="UI_TEST_USER")
+        username = test_user_credentials["username"]
+        password = test_user_credentials["password"]
+
+        page.locator(
+            "//div[contains(@class, 'visible-lg')]//input[@id='signInFormUsername']"
+        ).fill(username)
+        page.locator(
+            "//div[contains(@class, 'visible-lg')]//input[@id='signInFormPassword']"
+        ).fill(password)
+        page.locator(
+            "//div[contains(@class, 'visible-lg')]//input[@name='signInSubmitButton']"
+        ).click()
+
+    def logout(self, page):
+        print("Logging out")
+        self.click_button(page, "Log out")
+
+    def assert_on_cognito_login(self, page):
+        print("Checking that we are on the Cognito login page")
+        assert "amazoncognito.com/login" in page.url
+
 
 class TestUI(BaseTestUI):
-    def test_ui(self):
+    def test_log_in_and_log_out_sequence(self):
         with sync_playwright() as playwright:
             page = self.set_up_base_page(playwright)
-            print("--------- UI Tests ---------")
-            self.go_to(page, "/upload")
+
             # Should be redirected to log in when not authenticated
+            self.go_to(page, "/upload")
+
             self.assert_title(page, "rAPId - Login")
             self.click_link(page, "Log in to rAPId")
+
+            self.login_with_cognito(page)
+
+            self.assert_title(page, "rAPId - Upload")
+
+            # Should log out of Cognito session too and not redirect to /upload when logging in again
+            self.logout(page)
+            self.click_link(page, "Log in to rAPId")
+            self.assert_on_cognito_login(page)
