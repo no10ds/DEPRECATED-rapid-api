5d084754b9792a86074fd0737b5f9dadaf4e1f6e
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-08-11 09:38:27 +0100

    345-CP Add test for subject page controller

--- test/api/controller_ui/test_subject_management.py
+++ test/api/controller_ui/test_subject_management.py
@@ -6,6 +6,23 @@ from api.application.services.permissions_service import PermissionsService
 from test.api.common.controller_test_utils import BaseClientTest
 
 
+class TestSubjectPage(BaseClientTest):
+    @patch.object(Jinja2Templates, "TemplateResponse")
+    def test_calls_templating_engine_with_expected_arguments(
+        self, mock_templates_response
+    ):
+        subject_template_filename = "subject.html"
+
+        response = self.client.get("/subject", cookies={"rat": "user_token"})
+
+        mock_templates_response.assert_called_once_with(
+            name=subject_template_filename,
+            context={"request": ANY},
+        )
+
+        assert response.status_code == 200
+
+
 class TestCreateSubjectPage(BaseClientTest):
     @patch.object(PermissionsService, "get_ui_permissions")
     @patch.object(Jinja2Templates, "TemplateResponse")
