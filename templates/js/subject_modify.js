e17e57af3ee35e11902732f244c8903d624d2fce
jowita <jowita.podolak@digital.cabinet-office.gov.uk>
2022-08-12 15:17:49 +0100

    372-CP/JP Filter out NONE permissions

--- templates/js/subject_modify.js
+++ templates/js/subject_modify.js
@@ -3,11 +3,13 @@ const modify = () => {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]')
 
     const checkedRadios = Array.from(radios).filter(radio => radio.checked)
-    const checkedBoxes = Array.from(checkboxes).filter(radio => radio.checked)
+    const checkedBoxes = Array.from(checkboxes).filter(checkbox => checkbox.checked)
 
     const selections = checkedRadios.concat(checkedBoxes)
 
-    selections.forEach(element => {
+    const filteredSelections = selections.filter(selection => selection.value !== "NONE")
+
+    filteredSelections.forEach(element => {
         console.log(element.value)
     })
 }
