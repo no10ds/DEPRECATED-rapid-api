36473750e9174291136565644919075b1e008735
Lydiaade <lydia.adejumo@thoughtworks.com>
2022-08-15 09:59:38 +0100

    344-CD/LA Handle protected toggle when loading modify users

--- templates/js/common.js
+++ templates/js/common.js
@@ -1,29 +1,26 @@
 function toggle_element(do_toggle, element_to_toggle_id) {
-    element_to_toggle = document.getElementById(element_to_toggle_id)
-    element_to_toggle.hidden = do_toggle;
-    switch_enable_mode(do_toggle, element_to_toggle);
+  element_to_toggle = document.getElementById(element_to_toggle_id);
+  element_to_toggle.hidden = do_toggle;
+  switch_enable_mode(do_toggle, element_to_toggle);
 }
 
 function switch_enable_mode(do_switch, parent_element) {
-    [...parent_element.getElementsByTagName('input')].forEach(function(item) {
-      item.disabled = do_switch;
-    });
+  [...parent_element.getElementsByTagName("input")].forEach(function (item) {
+    item.disabled = do_switch;
+  });
 }
 
 function toggle_inputs(element_id, value_to_check, element_to_toggle) {
   document.getElementById(element_id).onchange = () => {
-    var name_to_bd = document.getElementById(element_id).value == value_to_check
+    var name_to_bd = document.getElementById(element_id).value == value_to_check;
     toggle_element(name_to_bd, element_to_toggle);
-  }
+  };
 }
 
-
-function toggle_check_boxes(radio_id, radio_name) {
-  if(radio_name == "GLOBAL_READ") {
-    toggle_element(radio_id == "READ_ALL", "READ_PROTECTED")
-  }
-
-  if(radio_name == "GLOBAL_WRITE") {
-    toggle_element(radio_id == "WRITE_ALL", "WRITE_PROTECTED")
+function toggle_check_boxes(radio_id) {
+  if (radio_id.startsWith("READ_") || radio_id.startsWith("GLOBAL_READ")) {
+    toggle_element(radio_id == "READ_ALL", "READ_PROTECTED");
+  } else if (radio_id.startsWith("WRITE_") || radio_id.startsWith("GLOBAL_WRITE")) {
+    toggle_element(radio_id == "WRITE_ALL", "WRITE_PROTECTED");
   }
 }
--- templates/js/subject_modify.js
+++ templates/js/subject_modify.js
@@ -1,26 +1,35 @@
+toggle_element(document.getElementById("WRITE_ALL").checked, "WRITE_PROTECTED");
+toggle_element(document.getElementById("READ_ALL").checked, "READ_PROTECTED");
+
 const modify = (subjectId) => {
-    const radios = document.querySelectorAll('input[type="radio"]')
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  const radios = document.querySelectorAll('input[type="radio"]');
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    const checkedRadios = Array.from(radios).filter(radio => radio.checked)
-    const checkedBoxes = Array.from(checkboxes).filter(checkbox => checkbox.checked)
+  const checkedRadios = Array.from(radios).filter((radio) => radio.checked);
+  const checkedBoxes = Array.from(checkboxes).filter(
+    (checkbox) => checkbox.checked
+  );
 
-    const selections = checkedRadios.concat(checkedBoxes)
+  const selections = checkedRadios.concat(checkedBoxes);
 
-    const filteredSelections = selections.filter(selection => selection.value !== "NONE")
+  const filteredSelections = selections.filter(
+    (selection) => selection.value !== "NONE"
+  );
 
-    const requestBody = {
-        "subject_id": subjectId,
-        "permissions": filteredSelections.map(element => element.value)
-    }
+  const requestBody = {
+    subject_id: subjectId,
+    permissions: filteredSelections.map((element) => element.value),
+  };
 
-    fetch("/client/permissions", {
-        method: "PUT",
-        // This is needed to treat this call as a browser request
-        headers: new Headers({'Accept': 'text/html,application/json', 'Content-Type': 'application/json'}),
-        body: JSON.stringify(requestBody)
-    }).then(_ => {
-        window.location.href = `${window.location.href}/success`
-    })
-
-}
+  fetch("/client/permissions", {
+    method: "PUT",
+    // This is needed to treat this call as a browser request
+    headers: new Headers({
+      Accept: "text/html,application/json",
+      "Content-Type": "application/json",
+    }),
+    body: JSON.stringify(requestBody),
+  }).then((_) => {
+    window.location.href = `${window.location.href}/success`;
+  });
+};
