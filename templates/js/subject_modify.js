aba8228bb420f2773e3ef97d70f46dd07dc5d49e
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-08-16 09:57:16 +0100

    372-JP/CP Add error handling to modify user page

--- templates/js/subject_modify.js
+++ templates/js/subject_modify.js
@@ -2,34 +2,66 @@ toggle_element(document.getElementById("WRITE_ALL").checked, "WRITE_PROTECTED");
 toggle_element(document.getElementById("READ_ALL").checked, "READ_PROTECTED");
 
 const modify = (subjectId) => {
-  const radios = document.querySelectorAll('input[type="radio"]');
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const radios = document.querySelectorAll('input[type="radio"]');
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-  const checkedRadios = Array.from(radios).filter((radio) => radio.checked);
-  const checkedBoxes = Array.from(checkboxes).filter(
-    (checkbox) => checkbox.checked
-  );
+    const checkedRadios = Array.from(radios).filter((radio) => radio.checked);
+    const checkedBoxes = Array.from(checkboxes).filter(
+        (checkbox) => checkbox.checked
+    );
 
-  const selections = checkedRadios.concat(checkedBoxes);
+    const selections = checkedRadios.concat(checkedBoxes);
 
-  const filteredSelections = selections.filter(
-    (selection) => selection.value !== "NONE"
-  );
+    const filteredSelections = selections.filter(
+        (selection) => selection.value !== "NONE"
+    );
 
-  const requestBody = {
-    subject_id: subjectId,
-    permissions: filteredSelections.map((element) => element.value),
-  };
+    const requestBody = {
+        subject_id: subjectId,
+        permissions: filteredSelections.map((element) => element.value),
+    };
 
-  fetch("/client/permissions", {
-    method: "PUT",
-    // This is needed to treat this call as a browser request
-    headers: new Headers({
-      Accept: "text/html,application/json",
-      "Content-Type": "application/json",
-    }),
-    body: JSON.stringify(requestBody),
-  }).then((_) => {
-    window.location.href = `${window.location.href}/success`;
-  });
+    fetch("/client/permissions", {
+        method: "PUT",
+        // This is needed to treat this call as a browser request
+        headers: new Headers({
+            Accept: "text/html,application/json",
+            "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(requestBody),
+    }).then(response => response.json()
+        .then(result => {
+            if (response.ok) {
+                navigateToSuccessPage()
+            } else {
+                showErrorMessage(result["details"])
+            }
+        })).catch(_ => {
+        showErrorMessage('Something went wrong. Please contact your system administrator.')
+    });
 };
+
+const navigateToSuccessPage = () => {
+    window.location.href = `${window.location.href}/success`;
+}
+
+const showErrorMessage = (message) => {
+    const errorMessageContainer = document.getElementById('error-message')
+    errorMessageContainer.innerText = message
+}
+
+const hideErrorMessage = () => {
+    const errorMessageContainer = document.getElementById('error-message')
+    errorMessageContainer.innerText = ''
+}
+
+const setupEventListeners = () => {
+    // Set up inputs to clear error message on interaction
+    const allInputs = document.querySelectorAll('input')
+    allInputs.forEach(input => {
+        input.addEventListener('click', hideErrorMessage)
+    })
+}
+
+
+setupEventListeners()
