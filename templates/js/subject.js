c91ef08d30f094ccdc345e1ae30c09eead5ef27e
Claude Paret <claude.paret@digital.cabinet-office.gov.uk>
2022-08-16 10:08:22 +0100

    373-JP/CP Disbale modify button until selection made

--- templates/js/subject.js
+++ templates/js/subject.js
@@ -2,3 +2,9 @@ const goToNextStep = () => {
     const subjectId = document.getElementById("select_subject").value
     window.location.href = `subject/${subjectId}/modify`
 }
+
+const toggleModifyButton = (event) => {
+    document.getElementById('button-modify').disabled = event.target.value === ''
+}
+
+document.getElementById('select_subject').addEventListener('change', toggleModifyButton)
