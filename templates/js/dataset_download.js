1704238c91170f50dc03f586e4a553ac4eb13e17
jowita <jowita.podolak@digital.cabinet-office.gov.uk>
2022-08-18 15:17:03 +0100

    381-CD/JP Download dataset functionality in the ui

---
+++ templates/js/dataset_download.js
@@ -0,0 +1,50 @@
+const downloadDataset = (domain, dataset) => {
+    const selected_format = get_selected_value("select_format");
+
+    const accept_header = select_format == "json" ? "application/json" : "text/csv"
+
+    // Empty SQL query
+    const queryBody = {}
+
+    fetch(`/datasets/${domain}/${dataset}/query`, {
+        method: "POST",
+        headers: new Headers({
+            "Accept": accept_header,
+            "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(queryBody),
+    }).then(response => response.blob()
+        .then(blob => {
+            if (response.ok) {
+                // disable submit button till the dataset downloads
+                toggleSwitchDisable('download-dataset', true)
+
+                downloadFile(blob, domain, dataset, selected_format)
+                // enable submit button back
+                toggleSwitchDisable('download-dataset', false)
+            } else {
+                showErrorMessage(result["details"])
+            }
+        })).catch(_ => {
+        showErrorMessage('Something went wrong. Please contact your system administrator.')
+        // enable submit button back
+        toggleSwitchDisable('download-dataset', false)
+    });
+};
+
+function toggleSwitchDisable(elementId, isDisabled) {
+    elementToToggle = document.getElementById(elementId);
+    elementToToggle.disabled = isDisabled
+}
+
+function downloadFile(blob, domain, dataset, selected_format) {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = `${domain}_${dataset}.${selected_format}`;
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+
+}
