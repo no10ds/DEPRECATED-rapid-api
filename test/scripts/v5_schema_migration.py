9ec5aa081f13591812db6a20d2c56016eea47fd5
Toby Drane <tdrane@no10.gov.uk>
2023-01-25 22:34:31 +0000

    paginate the migration script

--- test/scripts/v5_schema_migration.py
+++ test/scripts/v5_schema_migration.py
@@ -15,16 +15,18 @@ DATA_BUCKET = os.environ["DATA_BUCKET"]
 s3_client = boto3.client("s3")
 glue_client = boto3.client("glue", region_name=AWS_REGION)
 
+paginator = s3_client.get_paginator("list_objects_v2")
+pages = paginator.paginate(Bucket=DATA_BUCKET, Prefix="data/schemas", MaxKeys=10)
+keys = set()
+for page in pages:
+    for folder in page["Contents"]:
+        folder_key = folder["Key"]
+        folder_size = folder["Size"]
+        if folder_size != 0:
+            keys.add(folder_key)
 
-objects = s3_client.list_objects_v2(
-    Bucket=DATA_BUCKET,
-    Prefix="data/schemas",
-)["Contents"]
-
-for _object in objects:
-    res = s3_client.get_object(Bucket=DATA_BUCKET, Key=_object["Key"])
+for key in keys:
+    res = s3_client.get_object(Bucket=DATA_BUCKET, Key=key)
     json_object = json.loads(res["Body"].read().decode("utf-8"))
     json_object |= {"description": ""}
-    s3_client.put_object(
-        Body=json.dumps(json_object), Bucket=DATA_BUCKET, Key=_object["Key"]
-    )
+    s3_client.put_object(Body=json.dumps(json_object), Bucket=DATA_BUCKET, Key=key)
